1638c33fcce72bd10a3263e22f099e9d97821c2c
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-10-10 20:45:17 -0300

    feat: initializr project

---
+++ src/test/java/br/com/wesleymoraes/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesleymoraes.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bdbc0f75cfeb75bb614ca317ec04bb817e51b709
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-10-10 22:39:21 -0300

    feat: criated repository and inject dependency in controller user

--- src/main/java/br/com/wesleymoraes/todolist/user/UserController.java
+++ src/main/java/br/com/wesleymoraes/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.wesleymoraes.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        UserModel userName = this.userRepository.findByUserName(userModel.getUserName());
+
+        if (userName != null) {
+            // mensagem de erro
+            // status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        UserModel userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
     }
 
